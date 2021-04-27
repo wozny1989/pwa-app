fe8473e6fcd3f227263c617d8baaaa74159e6493
Adam Woźny <wozny1989@gmail.com>
2021-04-27 23:36:34 +0200

    edit notes possibiility

--- app/components/notes/component.js
+++ app/components/notes/component.js
@@ -1,25 +1,37 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { storageFor } from 'ember-local-storage';
+import { inject as service } from '@ember/service';
 
 export default class NotesIndexComponent extends Component {
   @storageFor('notes') notes;
+  @service store;
 
   @action
   addNote() {
     this.notes.addObject({
-      title: 'My title',
-      content: 'Content note',
+      title: 'Title',
+      content: 'Description',
     });
   }
 
   @action
-  removeNote(note) {
-    console.log(`removing note: ${note}`);
+  removeNote(index) {
+    this.notes.removeAt(index, 1);
   }
 
   @action
   clearAllNotes() {
     this.notes.clear();
+  }
+
+  @action
+  editValue(index, { target }) {
+    const { innerText } = target;
+    const { value } = target.attributes.name;
+
+    const updatedNote = this.notes.objectAt(index);
+    updatedNote[value] = innerText;
+    this.notes.replace(index, 1, [updatedNote]);
   }
 }
