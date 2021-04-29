95846589f05c1e25ef114a5226acb25e12ac6cb5
Adam Woźny <wozny1989@gmail.com>
2021-04-29 02:27:57 +0200

    implement logic for recalculate precent to bubble position

--- app/components/spirit-level/component.js
+++ app/components/spirit-level/component.js
@@ -25,6 +25,30 @@ export default class SpiritLevelComponent extends Component {
     this._setActiveScreen(false);
   }
 
+  get precent() {
+    if (this.beta > 100) {
+      return 50 - (180 - this.beta) * 10;
+    }
+
+    if (this.beta < -100) {
+      return 50 + (180 + Number(this.beta)) * 10;
+    }
+
+    return 50 - this.beta * 10;
+  }
+
+  get precentLimiter() {
+    if (this.precent > 95) {
+      return 96;
+    }
+
+    if (this.precent < 5) {
+      return 4;
+    }
+
+    return this.precent;
+  }
+
   _setActiveScreen(flag) {
     if (flag) {
       return navigator.wakeLock.request('screen').then((wakeLock) => {
